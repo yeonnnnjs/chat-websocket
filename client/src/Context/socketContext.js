85d5450cbe0aefc22017e58fe9a5387f93e65a43
yeonnnnjs <sungyeon52@gmail.com>
2024-05-19 00:16:21 +0900

    fix: modify duplicated room name issue

--- client/src/Context/socketContext.js
+++ client/src/Context/socketContext.js
@@ -2,8 +2,5 @@ import { createContext } from 'react';
 import { io } from "socket.io-client";
 
 export const socket = io.connect('http://localhost:3001', {
-    // headers: {
-    //   Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c`
-    // }
   });
 export const SocketContext = createContext(socket);
--- client/src/Main/Main.js
+++ client/src/Main/Main.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { socket } from '../Context/socketContext';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { socket } from "../Context/socketContext";
+import { useNavigate } from "react-router-dom";
 
 function Main() {
   const [rooms, setRooms] = useState([]);
@@ -8,52 +8,44 @@ function Main() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    socket.emit('getRooms', (data) => {
+    socket.emit("getRooms", (data) => {
       setRooms(data);
     });
-    socket.on('getRoomsChange', (data) => {
+    socket.on("getRoomsChange", (data) => {
       setRooms(data);
     });
-  }, []);
-
-  const joinRoom = (roomName) => {
-    socket.emit("joinRoom", roomName);
-    localStorage.setItem("roomName", roomName);
-    navigate(`/chat/`+roomName);
-  };
+  }, [rooms]);
 
   const handleCreateRoom = async () => {
     if (socket && roomName) {
-      let isRoomExist = false;
-      await socket.emit('isRoomExist', roomName, (data) => {
-        isRoomExist = data;
+      await socket.emit("createRoom", roomName, (data) => {
+        const isRoomExist = data.isRoomExist;
+        if (isRoomExist) {
+          alert("방 이미 있음 ㅇㅇ");
+        } else {
+          localStorage.setItem("roomName", roomName);
+          navigate(`/chat/${roomName}`);
+        }
       });
-      if (isRoomExist) {
-        alert("방 이미 있음 ㅇㅇ");
-      } 
-      else {
-        await socket.emit("createRoom", roomName);
-        localStorage.setItem("roomName", roomName);
-        navigate(`/chat/${roomName}`);
-      }
     }
   };
 
-  const handleJoinRoom = async () => {
+  const handleJoinRoom = async (roomName) => {
     if (socket && roomName) {
-      let isRoomExist = false;
-      await socket.emit('isRoomExist', roomName, (data) => {
-        isRoomExist = data;
+      await socket.emit("joinRoom", roomName, (data) => {
+        const isRoomExist = data.isRoomExist;
+        if (!isRoomExist) {
+          alert("그런 방 없음 ㅇㅇ");
+        } else {
+          localStorage.setItem("roomName", roomName);
+          navigate(`/chat/${roomName}`);
+        }
       });
-      if (!isRoomExist) {
-        alert("그런 방 없음 ㅇㅇ");
-      } 
-      else {
-        await socket.emit("joinRoom", roomName);
-        localStorage.setItem("roomName", roomName);
-        navigate(`/chat/${roomName}`);
-      }
     }
+  };
+
+  const handleRoomName = (e) => {
+    setRoomName(e.target.value);
   };
 
   return (
@@ -61,17 +53,23 @@ function Main() {
       <div className="form-container">
         <input
           value={roomName}
-          onChange={(e) => setRoomName(e.target.value)}
+          onChange={handleRoomName}
           placeholder="Enter room name"
         />
-        <button type="button" onClick={handleJoinRoom}>방 입장</button>
-        <button type="button" onClick={handleCreateRoom}>방 만들기</button>
+        <button type="button" onClick={() => handleJoinRoom(roomName)}>
+          방 입장
+        </button>
+        <button type="button" onClick={handleCreateRoom}>
+          방 만들기
+        </button>
       </div>
       <div className="list-container">
         {rooms.map((room) => (
           <div key={room.roomName} className="list-item">
             {room.roomName}
-            <button type="button" onClick={()=>joinRoom(room.roomName)}>입장</button>
+            <button type="button" onClick={() => handleJoinRoom(room.roomName)}>
+              입장
+            </button>
           </div>
         ))}
       </div>
